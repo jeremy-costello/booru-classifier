730dc10213125bb8b865b7ed25fe3e5660f292d3
Jeremy Costello <jeremy1@ualberta.ca>
2024-01-19 13:02:42 -0330

    fix parameters

--- parameters.py
+++ parameters.py
@@ -1,11 +1,12 @@
 parameter_dict = {
     "booru_url": "https://safebooru.org",
     "database_file": "./data/safebooru.db",
+    "skeleton_parquet_file": "./data/skeleton.parquet",
+    "tag_counts_json": "./data/tag_counts.json",
+    "tag_indices_json": "./data/tag_indices.json",
+    "dataset_statistics_json": "./data/dataset_statistics.json",
     "large_file_root": None,  # absolute file path
-    "skeleton_parquet_file": "skeleton.parquet",
-    "tag_counts_json": "tag_counts.json",
-    "tag_indices_json": "tag_indices.json",
-    "dataset_statistics_json": "dataset_statistics.json",
+    # large files ↓↓↓
     "train_parquet_file": "train.parquet",
     "valid_parquet_file": "valid.parquet",
     "image_tensorstore_file": "images_store.ts",
